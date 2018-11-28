b100edd421f0f2ea6168ce4e1b6cf1769756dd77
Romel levy <Romelestefan@gmail.com>
2018-11-28 14:40:50 -0500

    studentdao

--- src/main/java/co/groupproject/GCUniversity/dao/StudentDao.java
+++ src/main/java/co/groupproject/GCUniversity/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package co.groupproject.GCUniversity.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
@@ -7,9 +9,9 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
+import co.groupproject.GCUniversity.model.Course;
+import co.groupproject.GCUniversity.model.Enrollment;
 import co.groupproject.GCUniversity.model.Student;
-
-;
 
 @Repository
 @Transactional
@@ -18,15 +20,39 @@ public class StudentDao {
 	@PersistenceContext
 	private EntityManager em;
 
+	// Find by last name for login & to determine whether Admin or not.
 	public Student findByLastname(String lastname) {
 		try {
 			return em.createQuery("FROM Student" + " WHERE lastname = :lastname", Student.class)
 					.setParameter("lastname", lastname).getSingleResult();
 		} catch (NoResultException ex) {
-			// No user with that username found.
+			// No user with that last name found will return null.
 			return null;
 		}
 
+	}
+
+	// List Students
+	public List<Student> findAll() {
+		return em.createQuery("FROM Student", Student.class).getResultList();
+	}
+
+	// List Courses
+	public List<Enrollment> findByCourse(Course course) {
+		return em.createQuery("FROM Enrollment WHERE course = :course", Enrollment.class).setParameter("course", course)
+				.getResultList();
+	}
+
+	// List Grades
+	public List<Enrollment> findByGrade(String grade) {
+		return em.createQuery("FROM Enrollment WHERE grade = :grade", Enrollment.class).setParameter("grade", grade)
+				.getResultList();
+	}
+
+	// List Semesters
+	public List<Enrollment> findBySemester(String semester) {
+		return em.createQuery("FROM Enrollment WHERE semester = :semester", Enrollment.class)
+				.setParameter("semester", semester).getResultList();
 	}
 
 }
