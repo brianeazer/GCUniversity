89838c2bbee1c52ece979e9b13c2f464e09bb293
Michael Kincaid <mbkincaid.1@gmail.com>
2018-11-28 10:48:15 -0500

    skeleton of controller and jsp

---
+++ src/main/java/co/groupproject/GCUniversity/Controller.java
@@ -0,0 +1,62 @@
+package co.groupproject.GCUniversity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Controller
+public class Controller {
+	
+//	@Autowired
+//	String something;
+	
+	@RequestMapping ("/") 
+	public ModelAndView index() {
+		
+		
+		return new ModelAndView("home");
+	}
+	@RequestMapping ("/student-home") 
+	public ModelAndView studentHome() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+	@RequestMapping ("/student-enroll") 
+	public ModelAndView studentEnroll() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+	@RequestMapping ("/student-course") 
+	public ModelAndView studentCourse() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+	@RequestMapping ("/admin-courses") 
+	public ModelAndView adminCourses() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+	@RequestMapping ("/admin-course") 
+	public ModelAndView adminCourse() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+	@RequestMapping ("/course-list") 
+	public ModelAndView courseList() {
+		ModelAndView mv = new ModelAndView();
+		
+		
+		return mv;
+	}
+
+}
