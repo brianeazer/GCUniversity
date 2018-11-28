000f4ae72697a223e31800939eda81c01d5855cf
Romel levy <Romelestefan@gmail.com>
2018-11-28 11:22:16 -0500

    new stuff

--- src/main/java/co/groupproject/GCUniversity/model/Course.java
+++ src/main/java/co/groupproject/GCUniversity/model/Course.java
@@ -1,5 +1,47 @@
 package co.groupproject.GCUniversity.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String enrollment;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEnrollment() {
+		return enrollment;
+	}
+
+	public void setEnrollment(String enrollment) {
+		this.enrollment = enrollment;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", enrollment=" + enrollment + "]";
+	}
 
 }
--- src/main/java/co/groupproject/GCUniversity/model/Enrollment.java
+++ src/main/java/co/groupproject/GCUniversity/model/Enrollment.java
@@ -1,5 +1,65 @@
 package co.groupproject.GCUniversity.model;
 
-public class Enrollment {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Enrollment {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String student;
+	private String course;
+	private String grade;
+	private String semester;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getSemester() {
+		return semester;
+	}
+
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+	@Override
+	public String toString() {
+		return "Enrollment [id=" + id + ", student=" + student + ", course=" + course + ", grade=" + grade
+				+ ", semester=" + semester + "]";
+	}
 }
