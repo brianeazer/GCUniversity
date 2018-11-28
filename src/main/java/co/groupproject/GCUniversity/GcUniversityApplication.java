ebea9c557659e82a9b0099ea6d50d33c653fa9ea
Brian Eazer <brianeazer2015@gmail.com>
2018-11-28 10:19:42 -0500

    first commit

---
+++ src/main/java/co/groupproject/GCUniversity/GcUniversityApplication.java
@@ -0,0 +1,12 @@
+package co.groupproject.GCUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcUniversityApplication.class, args);
+	}
+}
